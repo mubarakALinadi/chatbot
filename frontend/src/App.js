7f05010adf04793afa3ef9e6eb07792ac4153f01
mubarakALinadi <Mubarak.alinadi@gmail.com>
2026-02-26 01:58:23 +0300

    update backend URL to EC2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,7 +21,7 @@ function App() {
     setThinking(true);
 
     try {
-      const res = await fetch('http://localhost:8000/chat', {
+      const res = await fetch('http://16.171.11.169:8000/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ messages: updatedMessages }),
